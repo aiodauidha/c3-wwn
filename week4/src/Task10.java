00ba39393332090a74f469d0a761d05c6ebf8c6b
pluto <3231679447@qq.com>
2021-02-26 17:20:27 +0800

    第十题

---
+++ week4/src/Task10.java
@@ -0,0 +1,39 @@
+public class Task10 {
+    public static void main(String[] args) {
+        Object obj = new Object();
+        Runnable A = new Runnable() {
+            @Override
+            public void run() {
+                synchronized (obj) {
+                    for (int i = 0; i < 6; i++) {
+                        obj.notify();
+                        System.out.println("线程A打印：" + 2 * i);
+                        try {
+                            obj.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        };
+        Runnable B = new Runnable() {
+            @Override
+            public void run() {
+                synchronized (obj) {
+                    for (int i = 0; i < 5; i++) {
+                        obj.notify();
+                        System.out.println("线程B打印：" + (2 * i + 1));
+                        try {
+                            obj.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        };
+        new Thread(A).start();
+        new Thread(B).start();
+    }
+}
