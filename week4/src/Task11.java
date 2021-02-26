cc436be05037aa4fd53a0e1ef9d0cd9689e53215
pluto <3231679447@qq.com>
2021-02-26 17:45:50 +0800

    第十一题

---
+++ week4/src/Task11.java
@@ -0,0 +1,80 @@
+class Message{
+    private String title;
+    private String content;
+    private boolean flag = true;
+    public synchronized void set(String title, String content) {
+        if (this.flag == false) {
+            try {
+                super.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        this.title = title;
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.content = content;
+        this.flag = false;
+        super.notify();
+    }
+    public synchronized void get() {
+        if (this.flag == true) {
+            try {
+                super.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(this.title + "-->" + this.content);
+        this.flag = true;
+        super.notify();
+    }
+}
+
+class Producer implements Runnable {
+    private Message msg = null;
+    public Producer(Message msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            if (i % 2 == 0) {
+                this.msg.set("李华","java讲师");
+            } else {
+                this.msg.set("mldn","www.mldnjava.com");
+            }
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    private Message msg = null;
+
+    public Consumer(Message msg) {
+        this.msg = msg;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            this.msg.get();
+        }
+    }
+}
+public class Task11 {
+    public static void main(String[] args) {
+        Message msg = new Message();
+        new Thread(new Producer(msg)).start();
+        new Thread(new Consumer(msg)).start();
+    }
+}
